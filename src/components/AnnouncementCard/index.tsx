ba0d6c21a5c15cb657277324f51b18b34b5f65b4
francisco <lucas.araripina@hotmail.com>
2022-12-14 12:11:56 -0300

    alterando rota de api

--- src/components/AnnouncementCard/index.tsx
+++ src/components/AnnouncementCard/index.tsx
@@ -27,7 +27,7 @@ interface CardProps {
 const AnnouncementCard: React.FC<CardProps> = (props) => {
   const navigate = useNavigate();
   const {addFavorite} = useUsers();
-  const img = 'http://15.229.1.158:3000/images/'+props.image;
+  const img = 'https://15.228.234.208/images/'+props.image;
   const {auth} = useAuth();
   const favoritar = ()=>{
     addFavorite(auth.user, {announcement: props.id, owner: props.owner.id});
