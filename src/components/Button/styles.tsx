5388d47ca50c2d542eb5cc274b9aa50777db57d7
JVSMOTA <joao.mota@ccc.ufcg.edu.br>
2023-07-06 10:41:32 -0300

    style: adjust button for Login and Signin

--- src/components/Button/styles.tsx
+++ src/components/Button/styles.tsx
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 export const StyledButton = styled.button`
   /* size */
   width: 100%;
-  height: 6vh;
+  height: 9vh;
 
   /* borders */
   border: none;
   box-sizing: border-box;
-  border-radius: 1vh;
+  border-radius: 10px;
 
   /* effects */
   cursor: pointer;
