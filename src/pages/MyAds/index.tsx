86d1633d536858edbef3c40d47244b606fc7ce21
JVSMOTA <joao.mota@ccc.ufcg.edu.br>
2023-07-18 11:08:04 -0300

    feat: component initial to my ads

---
+++ src/pages/MyAds/index.tsx
@@ -0,0 +1,62 @@
+import React, {useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import PageContainer from 'components/PageContainer';
+import Header from 'components/Header';
+import ContentBox from 'components/JustifyContainer';
+import JustifyContainer from 'components/JustifyContainer';
+import useAnnouncements from 'hooks/useAnnouncements';
+import useAuth from 'hooks/useAuth';
+import {ListOfCards} from 'pages/Announcements/styles';
+import AnnouncementCard from 'components/AnnouncementCard';
+import Footer from 'components/Footer';
+
+import {TitleArea} from './styles';
+import {TitleSection} from './styles';
+import {ContainerReturnToPage} from './styles';
+import ReturnToPage from 'assets/img/ReturnToPage.png';
+
+
+const MyAds: React.FC = () => {
+  const {auth} = useAuth();
+  const {announcements, fetchFavorites} = useAnnouncements();
+  useEffect(() => {
+    fetchFavorites(auth.user);
+  }, []);
+
+
+  return (
+    <PageContainer>
+      <Header />
+      <JustifyContainer thereIsHeader >
+        <TitleArea>
+          <TitleSection>
+            <ContainerReturnToPage>
+            <Link to="/Announcements">
+              <img src={ReturnToPage} />
+            </Link>
+          </ContainerReturnToPage>
+            Seus anúncios
+          </TitleSection>
+        </TitleArea>
+        <ContentBox>
+          <ListOfCards>
+            {announcements.map((announcement: any, index) => (
+              <AnnouncementCard
+                key={announcement.announcement.id}
+                id={announcement.announcementid}
+                title={announcement.announcement.title}
+                description={announcement.announcement.description}
+                image={announcement.announcement.images[0] as string}
+                localization={announcement.announcement.localization}
+                owner={announcement.announcement.owner}
+              />
+            ))}
+          </ListOfCards>
+        </ContentBox>
+      </JustifyContainer>
+      <Footer/>
+    </PageContainer>
+  );
+};
+
+export default MyAds;
