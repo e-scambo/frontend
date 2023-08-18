d3438a7426d119244afa3d79ce0c0d80637ac873
JVSMOTA <joao.mota@ccc.ufcg.edu.br>
2023-08-18 19:41:59 -0300

    feat: ajuste no componente ChatWindow para apresentar mensagens

--- src/components/ChatWindow/index.tsx
+++ src/components/ChatWindow/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import { Container, ChatHeader, UserProfile, UserName, BellIcon, Online } from './style';
 import SendMessage from 'components/SendMessage';
+import Message from 'components/Message';
 
 interface Props {
   children?: ReactNode;
@@ -8,18 +9,32 @@ interface Props {
 }
 
 const ChatWindow: React.FC<Props> = ({ children, chatId }) => {
+  // Exemplo de mensagens fictícias
+  const messages = [
+    { id: '1', content: 'Olá, como você está?', sender: 'user' as const },
+    { id: '2', content: 'Estou bem, obrigado!', sender: 'other' as const },
+    { id: '1', content: 'Olá, como você está?', sender: 'user' as const },
+    { id: '2', content: 'Estou bem, obrigado!', sender: 'other' as const },
+    { id: '1', content: 'Olá, como você está?', sender: 'user' as const },
+    { id: '2', content: 'Estou bem, obrigado!', sender: 'other' as const },
+    // ...
+  ];  
+
   return (
     <Container>
       <ChatHeader>
-        <UserProfile/>
+        <UserProfile />
         <UserName>Magno Lindo</UserName>
-        <Online/>
-        <BellIcon/>
+        <Online />
+        <BellIcon />
       </ChatHeader>
+      {messages.map(message => (
+        <Message key={message.id} content={message.content} sender={message.sender} />
+      ))}
       {children}
-      <SendMessage/>
+      <SendMessage />
     </Container>
-  )
+  );
 };
 
 export default ChatWindow;
