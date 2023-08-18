eaff2c314263095cbc9017fbfa08f33e847d2682
JVSMOTA <joao.mota@ccc.ufcg.edu.br>
2023-08-18 16:41:45 -0300

    fix: ajdust component to work

--- src/components/ChatWindow/index.tsx
+++ src/components/ChatWindow/index.tsx
@@ -1,12 +1,19 @@
 import React, { ReactNode } from 'react';
 import { Container } from './style';
+import SendMessage from 'components/SendMessage';
 
 interface Props {
   children?: ReactNode;
+  chatId: string;
 }
 
-const ChatWindow: React.FC<Props> = ({ children }) => {
-  return <Container>{children}</Container>;
+const ChatWindow: React.FC<Props> = ({ children, chatId }) => {
+  return (
+    <Container>
+      {children}
+      <SendMessage/>
+    </Container>
+  )
 };
 
 export default ChatWindow;
